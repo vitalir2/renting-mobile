3a21844b74b6f5a74384a2ed7eca6ac5c2c1207a
vitalir2 <xvostov2002@gmail.com>
2023-02-19 21:10:56 +0300

    RENTING-1: Move Compose dependencies to the version catalog

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -33,10 +33,6 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation(libs.bundles.androidx.compose.ui.common)
+    implementation(libs.androidx.activity.compose)
 }
