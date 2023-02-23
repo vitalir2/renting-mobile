bda98e8c543291a5e5535118ad857a529bc17710
vitalir2 <xvostov2002@gmail.com>
2023-02-23 10:53:01 +0300

    RENTING-3: [iOS] Add first Unit Test

--- iosApp/RentingAppTests/RentingAppTests.swift
+++ iosApp/RentingAppTests/RentingAppTests.swift
@@ -1,6 +1,6 @@
 //
-//  SampleTest.swift
-//  Renting App
+//  RentingAppTests.swift
+//  RentingAppTests
 //
 //  Created by Vitaly Khvostov on 23.02.2023.
 //  Copyright © 2023 Renting. All rights reserved.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-final class SampleTest: XCTestCase {
+final class RentingAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,18 +19,21 @@ final class SampleTest: XCTestCase {
     }
 
     func testExample() throws {
-        XCTAssertEqual(2 + 2, 4)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Mark your test throws to produce an unexpected failure
+        // when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete.
+        // Check the results with assertions afterwards.
+        XCTAssertEqual(2 + 2, 4)
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
+
 }
