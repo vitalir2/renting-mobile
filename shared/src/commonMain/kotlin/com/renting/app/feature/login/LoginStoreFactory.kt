e7e7f98f0097bbed11611b516d85e95dbfa59ae7
vitalir2 <xvostov2002@gmail.com>
2023-02-25 20:57:09 +0300

    RENTING-4: Integrate MVIKotlin

---
+++ shared/src/commonMain/kotlin/com/renting/app/feature/login/LoginStoreFactory.kt
@@ -0,0 +1,26 @@
+package com.renting.app.feature.login
+
+import com.arkivanov.mvikotlin.core.store.Reducer
+import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.core.store.create
+
+internal class LoginStoreFactory(private val storeFactory: StoreFactory) {
+
+    fun create(): LoginStore =
+        object : LoginStore,
+            Store<LoginStore.Intent, LoginStore.State, Nothing> by storeFactory.create(
+                name = "LoginStore",
+                initialState = LoginStore.State(),
+                reducer = ReducerImpl,
+            ) {}
+
+    private object ReducerImpl : Reducer<LoginStore.State, LoginStore.Intent> {
+
+        override fun LoginStore.State.reduce(msg: LoginStore.Intent): LoginStore.State =
+            when (msg) {
+                is LoginStore.Intent.SetLogin -> copy(login = login)
+                is LoginStore.Intent.SetPassword -> copy(password = password)
+            }
+    }
+}
