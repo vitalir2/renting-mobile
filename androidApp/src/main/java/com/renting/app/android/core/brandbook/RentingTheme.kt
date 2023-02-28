6ced49f58188d4eeb3d80303c8c86d11ac75c24a
vitalir2 <xvostov2002@gmail.com>
2023-02-28 21:24:25 +0300

    RENTING-5: Add and use light / dark colors for Android

--- androidApp/src/main/java/com/renting/app/android/core/brandbook/RentingTheme.kt
+++ androidApp/src/main/java/com/renting/app/android/core/brandbook/RentingTheme.kt
@@ -15,12 +15,31 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-private val darkPrimary = Color(0xFFBB86FC)
-private val darkPrimaryVariant = Color(0xFF3700B3)
-private val darkSecondary = Color(0xFF03DAC5)
-private val lightPrimary = Color(0xFF6200EE)
-private val lightPrimaryVariant = Color(0xFF3700B3)
-private val lightSecondary = Color(0xFF03DAC5)
+private object LightThemeColor {
+    val primary = Color(0xFF246BFD)
+    val primaryVariant = Color(0xFF264ABD)
+    val secondary = Color(0xFFE4ECFF)
+    val textSecondary = Color(0xFF616161)
+    val background = Color(0xFFFFFFFF)
+    val backgroundSecondary = Color(0xFFF5F5F5)
+    val onPrimary = Color(0xFFFFFFFF)
+    val onSecondary = primary
+    val onBackground = Color(0xFF212121)
+    val onBackgroundSecondary = Color(0xFF212121)
+}
+
+private object DarkThemeColor {
+    val primary = Color(0xFF246BFD)
+    val primaryVariant = Color(0xFF264ABD)
+    val secondary = Color(0xFF282A30)
+    val textSecondary = Color(0xFFE0E0E0)
+    val background = Color(0xFF181A20)
+    val backgroundSecondary = Color(0xFF1F222A)
+    val onPrimary = Color(0xFFFFFFFF)
+    val onSecondary = Color(0xFFFFFFFF)
+    val onBackground = Color(0xFFFFFFFF)
+    val onBackgroundSecondary = Color(0xFFFFFFFF)
+}
 
 @Composable
 fun RentingTheme(
@@ -29,15 +48,29 @@ fun RentingTheme(
 ) {
     val colors = if (darkTheme) {
         darkColors(
-            primary = darkPrimary,
-            primaryVariant = darkPrimaryVariant,
-            secondary = darkSecondary
+            primary = DarkThemeColor.primary,
+            primaryVariant = DarkThemeColor.primaryVariant,
+            secondary = DarkThemeColor.secondary,
+            secondaryVariant = DarkThemeColor.textSecondary,
+            background = DarkThemeColor.background,
+            surface = DarkThemeColor.backgroundSecondary,
+            onPrimary = DarkThemeColor.onPrimary,
+            onSecondary = DarkThemeColor.onSecondary,
+            onBackground = DarkThemeColor.onBackground,
+            onSurface = DarkThemeColor.onBackgroundSecondary,
         )
     } else {
         lightColors(
-            primary = lightPrimary,
-            primaryVariant = lightPrimaryVariant,
-            secondary = lightSecondary
+            primary = LightThemeColor.primary,
+            primaryVariant = LightThemeColor.primaryVariant,
+            secondary = LightThemeColor.secondary,
+            secondaryVariant = LightThemeColor.textSecondary,
+            background = LightThemeColor.background,
+            surface = LightThemeColor.backgroundSecondary,
+            onPrimary = LightThemeColor.onPrimary,
+            onSecondary = LightThemeColor.onSecondary,
+            onBackground = LightThemeColor.onBackground,
+            onSurface = LightThemeColor.onBackgroundSecondary,
         )
     }
     val typography = Typography(
