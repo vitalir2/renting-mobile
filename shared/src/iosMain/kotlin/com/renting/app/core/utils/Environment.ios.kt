f5af20b6fe12c14c053de312445d633c3076139c
vitalir2 <vitalyi100@mail.ru>
2023-03-10 15:26:47 +0300

    RENTING-6: Fix CI Build for iOS

--- shared/src/iosMain/kotlin/com/renting/app/core/utils/Environment.ios.kt
+++ shared/src/iosMain/kotlin/com/renting/app/core/utils/Environment.ios.kt
@@ -8,7 +8,8 @@ actual object Environment {
 
     @Suppress("UNCHECKED_CAST")
     private val config: Map<String, Any?> by lazy {
-        val configPath = NSBundle.mainBundle.pathForResource("EnvironmentConfig", ofType = "plist")
+        val configPath = getConfigPath("EnvironmentConfig")
+            ?: getConfigPath("DefaultEnvironmentConfig")
             ?: error("No environment config found")
         NSDictionary.dictionaryWithContentsOfFile(configPath) as Map<String, Any?>
     }
@@ -16,5 +17,9 @@ actual object Environment {
     actual val PRODUCTION_NETWORK_HOST: String by lazy {
         // TODO convert to typed config
         config["PRODUCTION_NETWORK_HOST"] as String
+    }
+
+    private fun getConfigPath(name: String): String? {
+        return NSBundle.mainBundle.pathForResource(name, ofType = "plist")
     }
 }
