7dd6a007acf47f6ab8c9d32c8700729ae7432ae4
vitalir2 <vitalyi100@mail.ru>
2023-03-11 11:25:40 +0300

    RENTING-24: [Android] Update layout for login screen

--- androidApp/src/main/java/com/renting/app/android/feature/login/LoginScreen.kt
+++ androidApp/src/main/java/com/renting/app/android/feature/login/LoginScreen.kt
@@ -1,25 +1,30 @@
 package com.renting.app.android.feature.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetpack.subscribeAsState
 import com.renting.app.android.core.brandbook.RentingTheme
+import com.renting.app.android.core.uikit.RentingButton
 import com.renting.app.feature.login.component.LoginComponent
 
 @Composable
@@ -35,6 +40,7 @@ fun LoginScreen(
             onLoginInputChanged = component::onLoginInputChanged,
             onPasswordInputChanged = component::onPasswordInputChanged,
             onSubmitButtonClick = component::onLoginStarted,
+            onSignUpClick = {}, // TODO
         )
         else -> SuccessTokenScreen(state)
     }
@@ -62,13 +68,29 @@ private fun ErrorPlaceholder(state: LoginComponent.Model) {
     }
 }
 
+private const val SIGN_UP_TAG = "SIGNUP"
+
+@OptIn(ExperimentalTextApi::class)
 @Composable
 private fun LoginScreen(
     model: LoginComponent.Model,
     onLoginInputChanged: (String) -> Unit,
     onPasswordInputChanged: (String) -> Unit,
     onSubmitButtonClick: () -> Unit,
+    onSignUpClick: () -> Unit,
 ) {
+    val primaryColor = MaterialTheme.colors.primary
+    val signUpText = buildAnnotatedString {
+        append("Don't have an account? ")
+        withAnnotation(tag = SIGN_UP_TAG, annotation = "") {
+            withStyle(
+                SpanStyle(color = primaryColor)
+            ) {
+                append("Sign up")
+            }
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -76,21 +98,42 @@ private fun LoginScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        TextField(
+        Text(
+            text = "Login to your account",
+            modifier = Modifier
+                .padding(16.dp),
+            style = MaterialTheme.typography.h4.copy(
+                fontWeight = FontWeight.Bold,
+            ),
+            textAlign = TextAlign.Center,
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        OutlinedTextField(
             modifier = Modifier
                 .testTag(LoginScreenTags.LOGIN_INPUT),
             value = model.login,
             onValueChange = onLoginInputChanged,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color(0xffF5F5F5),
+            ),
+            shape = RoundedCornerShape(size = 12.dp),
+            leadingIcon = {
+                Icon(
+                    Icons.Default.AccountCircle,
+                    contentDescription = null,
+                )
+            },
+            singleLine = true,
         )
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
+        Spacer(modifier = Modifier.height(16.dp))
+        PasswordInput(
+            password = model.password,
             modifier = Modifier
                 .testTag(LoginScreenTags.PASSWORD_INPUT),
-            value = model.password,
-            onValueChange = onPasswordInputChanged,
+            onInputChanged = onPasswordInputChanged,
         )
         Spacer(modifier = Modifier.height(8.dp))
-        Button(
+        RentingButton(
             onClick = onSubmitButtonClick,
             modifier = Modifier
                 .fillMaxWidth()
@@ -98,10 +141,67 @@ private fun LoginScreen(
         ) {
             Text("Login")
         }
+        ClickableText(
+            text = signUpText,
+            modifier = Modifier,
+            onClick = { offset ->
+                signUpText.getStringAnnotations(SIGN_UP_TAG, offset, offset)
+                    .firstOrNull()
+                    ?.let { onSignUpClick() }
+            },
+        )
     }
 }
 
-@Preview
+@Composable
+private fun PasswordInput(
+    password: String,
+    modifier: Modifier = Modifier,
+    onInputChanged: (String) -> Unit,
+) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = password,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color(0xffF5F5F5),
+        ),
+        shape = RoundedCornerShape(size = 12.dp),
+        onValueChange = onInputChanged,
+        leadingIcon = {
+            Icon(
+                Icons.Default.Lock,
+                contentDescription = null,
+            )
+        },
+        trailingIcon = {
+            Icon(
+                if (isPasswordVisible) {
+                    Icons.Filled.VisibilityOff
+                } else {
+                    Icons.Filled.Visibility
+                },
+                modifier = Modifier
+                    .clickable {
+                        isPasswordVisible = isPasswordVisible.not()
+                    },
+                contentDescription = null,
+            )
+        },
+        singleLine = true,
+        visualTransformation = if (isPasswordVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+    )
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xffffff,
+)
 @Composable
 private fun DefaultPreview() {
     RentingTheme {
@@ -113,6 +213,7 @@ private fun DefaultPreview() {
             onLoginInputChanged = {},
             onPasswordInputChanged = {},
             onSubmitButtonClick = {},
+            onSignUpClick = {},
         )
     }
 }
