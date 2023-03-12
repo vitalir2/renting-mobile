628c2cec5fdaa3f39a3c1778f3cb264fef0d2915
vitalir2 <vitalyi100@mail.ru>
2023-03-12 13:37:59 +0300

    RENTING-24: [Android] Refactor LoginScreen

---
+++ androidApp/src/main/java/com/renting/app/android/core/uikit/Gap.kt
@@ -0,0 +1,34 @@
+package com.renting.app.android.core.uikit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.renting.app.android.core.brandbook.RentingTheme
+
+// Scope param is used to define behavior
+@Suppress("UNUSED")
+@Composable
+@NonRestartableComposable
+fun ColumnScope.Gap(value: Dp) {
+    Spacer(Modifier.height(value))
+}
+
+@Preview
+@Composable
+private fun ColumnGapPreview() {
+    RentingTheme {
+        Column {
+            Text("Hello")
+            Gap(24.dp)
+            Text("World")
+        }
+    }
+}
