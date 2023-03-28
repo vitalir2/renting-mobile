f3976f7b98a90aaf802cfab08a614bf491cb6f55
vitalir2 <vitalyi100@mail.ru>
2023-03-28 18:27:49 +0300

    RENTING-36: DefaultRecommendationRepository

---
+++ shared/src/commonMain/kotlin/com/renting/app/feature/recommendation/DefaultRecommendationRepository.kt
@@ -0,0 +1,64 @@
+package com.renting.app.feature.recommendation
+
+import com.arkivanov.mvikotlin.logging.logger.DefaultLogger
+import com.renting.app.core.monad.Either
+import com.renting.app.core.monad.left
+import com.renting.app.core.monad.right
+import com.renting.app.core.network.CommonErrorResponse
+import com.renting.app.feature.property.PropertySnippet
+import com.renting.app.feature.property.PropertyType
+import com.renting.app.feature.property.serializer.PropertyTypeSerializer
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlin.math.roundToInt
+
+internal class DefaultRecommendationRepository(
+    private val httpClient: HttpClient,
+    private val ioDispatcher: CoroutineDispatcher,
+): RecommendationRepository {
+
+    override suspend fun getRecommendations(): Either<Exception, List<PropertySnippet>> = withContext(ioDispatcher) {
+        val result = httpClient.get("/api/announcements/open") {
+            contentType(ContentType.Application.Json)
+        }
+        if (result.status.isSuccess()) {
+            val recommendationsResponse = result.body<List<NetworkRecommendation>>()
+            recommendationsResponse.map(NetworkRecommendation::toDomainModel).right()
+        } else {
+            val errorBody = result.body<CommonErrorResponse>()
+            DefaultLogger.log(errorBody.message)
+            Exception(errorBody.message).left()
+        }
+    }
+}
+
+@Serializable
+private class NetworkRecommendation(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("description")
+    val description: String,
+    @SerialName("price")
+    val price: Double,
+    @SerialName("imagePathList")
+    val imagePathList: List<String>,
+    @SerialName("propertyType")
+    @Serializable(with = PropertyTypeSerializer::class)
+    val propertyType: PropertyType,
+)
+
+private fun NetworkRecommendation.toDomainModel(): PropertySnippet {
+    return PropertySnippet(
+        id = id,
+        type = propertyType,
+        location = description,
+        image = imagePathList.firstOrNull(),
+        price = price.roundToInt(),
+    )
+}
--- shared/src/commonMain/kotlin/com/renting/app/feature/recommendation/RecommendationRepository.kt
+++ shared/src/commonMain/kotlin/com/renting/app/feature/recommendation/RecommendationRepository.kt
@@ -1,8 +1,9 @@
 package com.renting.app.feature.recommendation
 
+import com.renting.app.core.monad.Either
 import com.renting.app.feature.property.PropertySnippet
 
 interface RecommendationRepository {
 
-    suspend fun getRecommendations(): List<PropertySnippet>
+    suspend fun getRecommendations(): Either<Exception, List<PropertySnippet>>
 }
