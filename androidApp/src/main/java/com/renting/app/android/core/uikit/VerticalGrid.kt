53cbf383a77148bfd634e537da3ef38c6a6399e6
vitalir2 <vitalyi100@mail.ru>
2023-04-02 07:50:39 +0300

    RENTING-42: [Android] Replace LazyGrid by the custom one

---
+++ androidApp/src/main/java/com/renting/app/android/core/uikit/VerticalGrid.kt
@@ -0,0 +1,48 @@
+package com.renting.app.android.core.uikit
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+
+@Composable
+fun VerticalGrid(
+    columns: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    require(columns > 0) { "Number of columns must be greater than 0" }
+
+    Layout(
+        content = content,
+        modifier = modifier,
+    ) { measurables, constraints ->
+        var layoutHeight = 0
+        val columnWidth = constraints.maxWidth / columns
+        val measurableConstraints = constraints.copy(
+            minWidth = columnWidth, maxWidth = columnWidth,
+        )
+        val placeables = measurables.mapIndexed { index, measurable ->
+            measurable.measure(measurableConstraints).also { placeable ->
+                val isFirstItemInRow = index % columns == 0
+                if (isFirstItemInRow) {
+                    layoutHeight += placeable.height
+                }
+            }
+        }
+        
+        layout(constraints.maxWidth, layoutHeight) {
+            var xPosition = 0
+            var yPosition = 0
+            placeables.forEachIndexed { index, placeable ->
+                placeable.placeRelative(x = xPosition, y = yPosition)
+                val isLastRowItem = (index + 1) % columns == 0
+                if (isLastRowItem) {
+                    xPosition = 0
+                    yPosition += placeable.height
+                } else {
+                    xPosition += placeable.width
+                }
+            }
+        }
+    }
+}
--- androidApp/src/main/java/com/renting/app/android/feature/property/PropertySnippetsGrid.kt
+++ androidApp/src/main/java/com/renting/app/android/feature/property/PropertySnippetsGrid.kt
@@ -1,15 +1,13 @@
 package com.renting.app.android.feature.property
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridScope
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.renting.app.android.core.uikit.RentingPreviewContainer
+import com.renting.app.android.core.uikit.VerticalGrid
 import com.renting.app.feature.property.PropertySnippet
 
 @Composable
@@ -17,14 +15,12 @@ fun PropertySnippetsGrid(
     snippets: List<PropertySnippet>,
     modifier: Modifier = Modifier,
     onSnippetClick: (id: Long) -> Unit,
-    prependedItems: LazyGridScope.() -> Unit = {},
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
+    VerticalGrid(
+        columns = 2,
         modifier = modifier,
     ) {
-        prependedItems()
-        items(snippets, key = PropertySnippet::id) { snippet ->
+        snippets.forEach { snippet ->
             PropertySnippetCard(
                 snippet = snippet,
                 onClick = { onSnippetClick(snippet.id) },
