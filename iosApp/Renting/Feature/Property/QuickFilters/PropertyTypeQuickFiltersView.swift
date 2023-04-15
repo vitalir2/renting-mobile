79f19a1d615cd5ed8ee315aee60ec766f78ed9b6
vitalir2 <vitalyi100@mail.ru>
2023-04-15 08:38:19 +0300

    RENTING-43: [iOS] PropertyTypeQuickFiltersView

---
+++ iosApp/Renting/Feature/Property/QuickFilters/PropertyTypeQuickFiltersView.swift
@@ -0,0 +1,51 @@
+//
+//  PropertyTypeQuickFilters.swift
+//  Renting
+//
+//  Created by Vitaly Khvostov on 15.04.2023.
+//  Copyright © 2023 Renting. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct PropertyTypeQuickFiltersView: View {
+    let filters: PropertyTypeQuickFilters
+    let onFilterSelected: (PropertyType) -> Void
+    let clearSelectedFilters: () -> Void
+    
+    var body: some View {
+        ScrollView(Axis.Set.horizontal, showsIndicators: false) {
+            HStack {
+                QuickFilter(
+                    name: "✅ All",
+                    isApplied: filters.appliedFilter == nil,
+                    onSelected: clearSelectedFilters
+                )
+                ForEach(filters.list, id: \PropertyTypeQuickFilter.type) { filter in
+                    PropertyTypeQuickFilterView(
+                        quickFilter: filter,
+                        onSelected: {
+                            onFilterSelected(filter.type)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+struct PropertyTypeQuickFilters_Previews: PreviewProvider {
+    static var previews: some View {
+        PropertyTypeQuickFiltersView(
+            filters: PropertyTypeQuickFilters(
+                list: PropertyTypeQuickFilters.companion.filtersOrder
+                    .map { type in
+                        PropertyTypeQuickFilter(type: type, isApplied: false)
+                    }
+            ),
+            onFilterSelected: { _ in },
+            clearSelectedFilters: {}
+        )
+    }
+}
