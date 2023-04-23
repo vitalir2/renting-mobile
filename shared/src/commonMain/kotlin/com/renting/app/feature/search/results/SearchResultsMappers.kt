c886fd67dc5927787387f06fdf61c0412009e351
vitalir2 <vitalyi100@mail.ru>
2023-04-23 11:52:50 +0300

    RENTING-48: Fix filtering

--- shared/src/commonMain/kotlin/com/renting/app/feature/search/results/SearchResultsMappers.kt
+++ shared/src/commonMain/kotlin/com/renting/app/feature/search/results/SearchResultsMappers.kt
@@ -3,9 +3,15 @@ package com.renting.app.feature.search.results
 internal object SearchResultsMappers {
 
     val stateToModel: SearchResultsStore.State.() -> SearchResultsComponent.Model = {
+        val appliedFilter = quickFilters?.appliedFilter
         SearchResultsComponent.Model(
             quickFilters = quickFilters,
-            searchState = searchState,
+            searchState = if (searchState is SearchState.Results && appliedFilter != null) {
+                val filteredSnippets = appliedFilter.applyTo(searchState.snippets)
+                searchState.copy(snippets = filteredSnippets)
+            } else {
+                searchState
+            },
         )
     }
 }
