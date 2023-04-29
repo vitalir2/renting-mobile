07a2033791edffe5850e4f519780050344cbb11a
vitalir2 <vitalyi100@mail.ru>
2023-04-29 16:53:31 +0300

    RENTING-52: PropertyGraph

--- shared/src/commonMain/kotlin/com/renting/app/feature/root/di/RootGraph.kt
+++ shared/src/commonMain/kotlin/com/renting/app/feature/root/di/RootGraph.kt
@@ -3,6 +3,7 @@ package com.renting.app.feature.root.di
 import com.renting.app.core.auth.di.AuthGraph
 import com.renting.app.feature.home.HomeGraph
 import com.renting.app.feature.login.di.LoginGraph
+import com.renting.app.feature.property.PropertyGraph
 import com.renting.app.feature.recommendation.RecommendationGraph
 import com.renting.app.feature.registration.di.RegistrationGraph
 import com.renting.app.feature.search.di.SearchGraph
@@ -20,4 +21,6 @@ interface RootGraph {
     val recommendationGraph: RecommendationGraph
 
     val searchGraph: SearchGraph
+
+    val propertyGraph: PropertyGraph
 }
