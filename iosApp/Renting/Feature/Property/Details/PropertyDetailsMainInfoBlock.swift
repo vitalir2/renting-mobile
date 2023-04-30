4451e7b45b5a6de2ab0fb3ae92e183d0f5796c88
vitalir2 <vitalyi100@mail.ru>
2023-04-30 15:52:07 +0300

    RENTING-52: [iOS] Example view with UI details

--- iosApp/Renting/Feature/Property/Details/PropertyDetailsMainInfoBlock.swift
+++ iosApp/Renting/Feature/Property/Details/PropertyDetailsMainInfoBlock.swift
@@ -7,15 +7,30 @@
 //
 
 import SwiftUI
+import shared
 
 struct PropertyDetailsMainInfoBlock: View {
+    let mainInfo: ComponentPropertyDetails.MainInfo
+    
     var body: some View {
-        Text("")
+        VStack(alignment: .leading) {
+            Text(mainInfo.price)
+                .font(.headline)
+            Spacer()
+                .frame(height: 8)
+            Text(mainInfo.area)
+                .font(.body)
+            Text("area")
+                .font(.caption)
+                .foregroundColor(Color.textSecondary)
+        }
     }
 }
 
 struct PropertyDetailsMainInfoBlock_Previews: PreviewProvider {
     static var previews: some View {
-        PropertyDetailsMainInfoBlock()
+        PropertyDetailsMainInfoBlock(
+            mainInfo: ComponentPropertyDetails.companion.preview.mainInfo
+        )
     }
 }
