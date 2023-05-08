f3f691822cb1a8f02d687fb8ca7d709cdc15f42c
vitalir2 <vitalyi100@mail.ru>
2023-05-08 14:56:12 +0300

    RENTING-54: [iOS] Design owner block

--- iosApp/Renting/Feature/Property/Details/PropertyDetailsOwnerBlock.swift
+++ iosApp/Renting/Feature/Property/Details/PropertyDetailsOwnerBlock.swift
@@ -11,11 +11,34 @@ import shared
 
 struct PropertyDetailsOwnerBlock: View {
     let ownerInfo: ComponentPropertyDetails.OwnerInfo
+    let onPhoneClicked: () -> Void
     
     var body: some View {
         HStack {
+            Spacer()
             UserAvatar(avatar: ownerInfo.avatar, fullName: ownerInfo.fullName)
                 .frame(width: 64, height: 64)
+            VStack(alignment: .leading) {
+                Text(ownerInfo.fullName)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                Spacer().frame(height: 6)
+                Text("Owner")
+                    .font(.caption)
+                    .fontWeight(.light)
+            }
+            Spacer()
+            Button(
+                action: {
+                    onPhoneClicked()
+                },
+                label: {
+                    Image(systemName: "phone")
+                        .tint(Color.appPrimary)
+                }
+            )
+            .padding(16)
+            Spacer()
         }
     }
 }
@@ -23,7 +46,8 @@ struct PropertyDetailsOwnerBlock: View {
 struct PropertyDetailsOwnerBlock_Previews: PreviewProvider {
     static var previews: some View {
         PropertyDetailsOwnerBlock(
-            ownerInfo: ComponentPropertyDetailsPreviews().apartment.ownerInfo
+            ownerInfo: ComponentPropertyDetailsPreviews().apartment.ownerInfo,
+            onPhoneClicked: {}
         )
     }
 }
