415330e3bf70581a612ab9bda32ccc2094376d53
vitalir2 <vitalyi100@mail.ru>
2023-07-17 20:31:10 +0300

    RENTING-66: Remove redundant imports

--- androidApp/src/main/java/com/renting/app/android/feature/property/PropertySnippetsGrid.kt
+++ androidApp/src/main/java/com/renting/app/android/feature/property/PropertySnippetsGrid.kt
@@ -1,6 +1,5 @@
 package com.renting.app.android.feature.property
 
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
