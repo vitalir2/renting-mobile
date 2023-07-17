7784d85aad4d5f3415aeacca0458cc4636b848ec
vitalir2 <vitalyi100@mail.ru>
2023-07-17 20:31:10 +0300

    RENTING-66: Remove redundant imports

--- androidApp/src/main/java/com/renting/app/android/feature/property/details/PropertyDetailsHeader.kt
+++ androidApp/src/main/java/com/renting/app/android/feature/property/details/PropertyDetailsHeader.kt
@@ -2,7 +2,6 @@ package com.renting.app.android.feature.property.details
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
@@ -13,7 +12,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
